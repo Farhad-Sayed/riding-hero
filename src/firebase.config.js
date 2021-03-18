c839be9fb70257956a6732b88b3791de8326d2dc
Farhad-Sayed <sayedtheprogrammer@gmail.com>
2021-03-19 00:30:21 +0600

    initializing riding-hero project

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAI-ZPewqoDtKyYWquLSO7d7Y82Fg4yx1g",
+    authDomain: "riding-hero.firebaseapp.com",
+    projectId: "riding-hero",
+    storageBucket: "riding-hero.appspot.com",
+    messagingSenderId: "973001602181",
+    appId: "1:973001602181:web:472ac41806aba571e5a5ec"
+  };
+
+  export default firebaseConfig;
